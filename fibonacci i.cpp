c9054234f4df9034bb38ec10c660274a20578c9f
deepika9493 <100104713+deepika9493@users.noreply.github.com>
2022-12-17 17:44:41 +0530

    Add files via upload

---
+++ fibonacci i.cpp
@@ -0,0 +1,22 @@
+//Deepika Mullgura
+//cst_spl2 (33)
+//fibonacci iterative
+#include <iostream>
+using namespace std;
+void fib(int num) {
+   int x = 0, y = 1, z = 0;
+   for (int i = 0; i < num; i++) {
+      cout << x << " ";
+      z = x + y;
+      x = y;
+      y = z;
+   }
+}
+int main() {
+   int num;
+   cout << "Enter the number : ";
+   cin >> num;
+   cout << "\nThe fibonacci series : " ;
+   fib(num);
+   return 0;
+}
